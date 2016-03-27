e00a00e3ea24d4c271a36ed454dc78f43e4ee719
Sergey Sharapov <ejilur@gmail.com>
2016-03-27 16:25:43 +0000

    Added paperclip gem and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326025041) do
+ActiveRecord::Schema.define(version: 20160327162249) do
+
+  create_table "post_attachments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "views"
     t.datetime "posted"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
